ab8f4b2b868b3f77a15234d73a053e5713e59f88
ZhouQiJun <zhouqijun4job@163.com>
2025-04-26 14:12:27 +0800

    chore: prettier 配置增加类型提示

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,8 +2,13 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-04-25 23:00:07
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-04-26 00:39:40
+ * @LastEditTime: 2025-04-26 14:11:32
  * @Description : prettier 配置
+ */
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
  */
 module.exports = {
   //"$schema": "https://json.schemastore.org/prettierrc",
